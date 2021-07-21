cdd78e1116a67cbdba6aaea0f5233e22f17b31f7
HemaVyas <Hema@hkls-mbp.lan>
2021-07-20 22:55:52 -0400

    working map

--- app.py
+++ app.py
@@ -4,26 +4,28 @@ import numpy as np
 from us_states import statesData
 import pandas as pd
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 # Create a Session Object to Connect to DB
 # ----------------------------------
 # Session is a temporary binding to our DB
 from sqlalchemy.orm import Session
 
-import json
+
 
 app = Flask(__name__)
 
 #################################################
 # Database Setup
 #################################################
-rds_connection_string = "postgres:postgres@localhost:5432/migration_db"
+rds_connection_string = "postgres:Hema@localhost:5432/migration_db"
 engine = create_engine(f'postgresql://{rds_connection_string}')
 
 Base = automap_base()
 
 Base.prepare(engine, reflect=True)
+
+print(Base.classes.keys())
 
 Relo = Base.classes.relocation
 
@@ -33,39 +35,94 @@ session = Session(engine)
 def index():
     return render_template("index.html")
 
-@app.route('/api/statemigration')
-def api_pull():
-    
 
-    data = session.query(Relo).all()
-
+@app.route('/api/test/<sel_state>')
+def api_pull2(sel_state):
+    sel_state = sel_state.upper()
+    rds_connection_string = "postgres:Hema@localhost:5432/migration_db"
+    engine = create_engine(f'postgresql://{rds_connection_string}')
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Relo = Base.classes.relocation
+    session = Session(engine)
+    data = session.query(Relo).filter(func.upper(Relo.primary_state)==sel_state).all()
     relo_list = []
-
     for row in data:
         dict = {}
-        dict["primary_state"] = row.primary_state
         dict["secondary_state"] = row.secondary_state
         dict["inflow"] = row.inflow
         dict["outflow"] = row.outflow
         relo_list.append(dict)
-        
     state_features = statesData["features"]
-
     for doc in state_features:
-        feature_list = []
         for item in relo_list:
             if item["secondary_state"] == doc["properties"]["name"]:
-                dict = {}
-                dict["primary_state"] = item["primary_state"]
-                dict["inflow"] = item["inflow"]
-                dict["outflow"] = item["outflow"]
-                feature_list.append(dict)
-        doc["properties"]["flows"] = feature_list
+                doc["properties"]["inflow"] = item["inflow"]
+                doc["properties"]["outflow"] = item["outflow"]
     statesData["features"] = state_features
-
     session.close()
-
     return(jsonify(statesData))
+
+
+@app.route('/api/statemigration/CA')
+def api_pull():
+    rds_connection_string = "postgres:Hema@localhost:5432/migration_db"
+    engine = create_engine(f'postgresql://{rds_connection_string}')
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Relo = Base.classes.relocation
+    session = Session(engine)
+    data = session.query(Relo).filter(Relo.primary_state=='California').all()
+    relo_list = []
+    for row in data:
+        dict = {}
+        dict["secondary_state"] = row.secondary_state
+        dict["inflow"] = row.inflow
+        dict["outflow"] = row.outflow
+        relo_list.append(dict)
+    state_features = statesData["features"]
+    for doc in state_features:
+        for item in relo_list:
+            if item["secondary_state"] == doc["properties"]["name"]:
+                doc["properties"]["inflow"] = item["inflow"]
+                doc["properties"]["outflow"] = item["outflow"]
+    statesData["features"] = state_features
+    session.close()
+    return(jsonify(statesData))
+
+# @app.route('/api/statemigration')
+# def api_pull():
+    
+
+#     data = session.query(Relo).all()
+
+#     relo_list = []
+
+#     for row in data:
+#         dict = {}
+#         dict["primary_state"] = row.primary_state
+#         dict["secondary_state"] = row.secondary_state
+#         dict["inflow"] = row.inflow
+#         dict["outflow"] = row.outflow
+#         relo_list.append(dict)
+        
+#     state_features = statesData["features"]
+
+#     for doc in state_features:
+#         feature_list = []
+#         for item in relo_list:
+#             if item["secondary_state"] == doc["properties"]["name"]:
+#                 dict = {}
+#                 dict["primary_state"] = item["primary_state"]
+#                 dict["inflow"] = item["inflow"]
+#                 dict["outflow"] = item["outflow"]
+#                 feature_list.append(dict)
+#         doc["properties"]["flows"] = feature_list
+#     statesData["features"] = state_features
+
+#     session.close()
+
+#     return(jsonify(statesData))
 
 
     # document = {}
--- static/js/logic.js
+++ static/js/logic.js
@@ -9,12 +9,53 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 L.geoJson(statesData).addTo(map);
 
-var selected_state = ""
-var api = "";
+var selected_state = d3.select("#indiana");
 
-d3.json("/data/sample_data.json").then(
-    function(data)
-    {
+var sample_data;
+var state_name
+var state_value
 
-    }
-);
+d3.json("/api/statemigration/CA").then(
+    function(data){
+    // {   console.log(data.features[0].properties.name);
+        // state_name = data.features[0].properties.name
+        
+        // console.log(data.features[0].properties.flows[2].inflow);
+        // state_value = data.features[0].properties.flows[2].inflow;
+
+        function getColor(c) {
+            return c > 80500 ? '#800026' :
+                   c > 69000  ? '#BD0026' :
+                   c > 57500  ? '#E31A1C' :
+                   c > 46000  ? '#FC4E2A' :
+                   c > 34500  ? '#FD8D3C' :
+                   c > 23000  ? '#FEB24C' :
+                   c > 11000  ? '#FED976' :
+                              '#FFEDA0';
+        }
+
+        function style(feature) {
+            // var c = 1000;
+            // state_name = data.features[0].properties.name
+            // state_value = feature.properties.flows[2].inflow;
+            state_value = feature.properties.inflow;
+        
+            return{
+                fillColor: getColor(state_value),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7
+        
+            };
+
+             
+        }
+        L.geoJson(data, {style: style}).addTo(map);
+    })
+
+    
+    
+    
+
