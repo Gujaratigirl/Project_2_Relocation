4e7371d9c96d6bff4e105fd1d9551fdbc08cde4b
Alivia Varvel <atvarvel@gmail.com>
2021-07-21 19:35:46 -0400

    updated for heroku

--- relocation/app.py
+++ relocation/app.py
@@ -3,62 +3,63 @@ from flask import Flask, render_template, jsonify
 import us_states
 import os
 
-from sqlalchemy import create_engine, func
-from sqlalchemy.ext.automap import automap_base
-# Create a Session Object to Connect to DB
-# ----------------------------------
-# Session is a temporary binding to our DB
-from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
+# from sqlalchemy.ext.automap import automap_base
+# # Create a Session Object to Connect to DB
+# # ----------------------------------
+# # Session is a temporary binding to our DB
+# from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 
 #################################################
 # Database Setup
 #################################################
-rds_connection_string = os.environ.get('DATABASE_URL', '') or "postgresql://postgres:postgres@localhost:5432/migration_db"
-engine = create_engine(f'{rds_connection_string}')
+# rds_connection_string = os.environ.get('DATABASE_URL', '') or "postgresql://postgres:postgres@localhost:5432/migration_db"
+# engine = create_engine(f'{rds_connection_string}')
 
-Base = automap_base()
+# Base = automap_base()
 
-Base.prepare(engine, reflect=True)
+# Base.prepare(engine, reflect=True)
 
-Relo = Base.classes.relocation
+# Relo = Base.classes.relocation
 
-session = Session(engine)
+# session = Session(engine)
 
-statesData = us_states.statesData
+# statesData = us_states.statesData
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/api/test/<state_name>')
-def api_pull2(state_name):
-    state_name = state_name.upper()
-    # rds_connection_string = "postgres:postgres@localhost:5432/migration_db"
-    # engine = create_engine(f'postgresql://{rds_connection_string}')
-    # Base = automap_base()
-    # Base.prepare(engine, reflect=True)
+# @app.route('/api/test/<state_name>')
+# def api_pull2(state_name):
+#     state_name = state_name.upper()
+#     # rds_connection_string = "postgres:postgres@localhost:5432/migration_db"
+#     # engine = create_engine(f'postgresql://{rds_connection_string}')
+#     # Base = automap_base()
+#     # Base.prepare(engine, reflect=True)
     
-    # session = Session(engine)
-    data = session.query(Relo).filter(func.upper(Relo.primary_state)==state_name).all()
-    relo_list = []
-    for row in data:
-        dict = {}
-        dict["secondary_state"] = row.secondary_state
-        dict["inflow"] = row.inflow
-        dict["outflow"] = row.outflow
-        relo_list.append(dict)
-    state_features = statesData["features"]
-    for doc in state_features:
-        for item in relo_list:
-            if item["secondary_state"] == doc["properties"]["name"]:
-                doc["properties"]["inflow"] = item["inflow"]
-                doc["properties"]["outflow"] = item["outflow"]
-    statesData["features"] = state_features
-    session.close()
-    return(jsonify(statesData))
+#     # session = Session(engine)
+#     data = session.query(Relo).filter(func.upper(Relo.primary_state)==state_name).all()
+#     relo_list = []
+#     for row in data:
+#         dict = {}
+#         dict["secondary_state"] = row.secondary_state
+#         dict["inflow"] = row.inflow
+#         dict["outflow"] = row.outflow
+#         relo_list.append(dict)
+#     state_features = statesData["features"]
+#     for doc in state_features:
+#         for item in relo_list:
+#             if item["secondary_state"] == doc["properties"]["name"]:
+#                 doc["properties"]["inflow"] = item["inflow"]
+#                 doc["properties"]["outflow"] = item["outflow"]
+#     statesData["features"] = state_features
+#     session.close()
+
+#     return(jsonify(statesData))
 
 
 # @app.route('/api/statemigration/CA')
