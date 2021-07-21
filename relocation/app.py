a344a40895b14dce713931a0654edc34f004f3c4
Alivia Varvel <atvarvel@gmail.com>
2021-07-21 16:35:53 -0400

    updated files for heroku deployment

--- relocation/app.py
+++ relocation/app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template, jsonify
 
 import os
 import numpy as np
-from .us_states import statesData
+from us_states import statesData
 import pandas as pd
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 # Create a Session Object to Connect to DB
 # ----------------------------------
@@ -34,39 +34,93 @@ session = Session(engine)
 def index():
     return render_template("index.html")
 
-@app.route('/api/statemigration')
-def api_pull():
-    
-
-    data = session.query(Relo).all()
-
+@app.route('/api/test/<state_name>')
+def api_pull2(state_name):
+    state_name = state_name.upper()
+    rds_connection_string = "postgres:postgres@localhost:5432/migration_db"
+    engine = create_engine(f'postgresql://{rds_connection_string}')
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Relo = Base.classes.relocation
+    session = Session(engine)
+    data = session.query(Relo).filter(func.upper(Relo.primary_state)==state_name).all()
     relo_list = []
-
     for row in data:
         dict = {}
-        dict["primary_state"] = row.primary_state
         dict["secondary_state"] = row.secondary_state
         dict["inflow"] = row.inflow
         dict["outflow"] = row.outflow
         relo_list.append(dict)
-        
     state_features = statesData["features"]
-
     for doc in state_features:
-        feature_list = []
         for item in relo_list:
             if item["secondary_state"] == doc["properties"]["name"]:
-                dict = {}
-                dict["primary_state"] = item["primary_state"]
-                dict["inflow"] = item["inflow"]
-                dict["outflow"] = item["outflow"]
-                feature_list.append(dict)
-        doc["properties"]["flows"] = feature_list
+                doc["properties"]["inflow"] = item["inflow"]
+                doc["properties"]["outflow"] = item["outflow"]
     statesData["features"] = state_features
-
     session.close()
-
     return(jsonify(statesData))
+
+
+@app.route('/api/statemigration/CA')
+def api_pull():
+    rds_connection_string = "postgres:postgres@localhost:5432/migration_db"
+    engine = create_engine(f'postgresql://{rds_connection_string}')
+    Base = automap_base()
+    Base.prepare(engine, reflect=True)
+    Relo = Base.classes.relocation
+    session = Session(engine)
+    data = session.query(Relo).filter(Relo.primary_state=='California').all()
+    relo_list = []
+    for row in data:
+        dict = {}
+        dict["secondary_state"] = row.secondary_state
+        dict["inflow"] = row.inflow
+        dict["outflow"] = row.outflow
+        relo_list.append(dict)
+    state_features = statesData["features"]
+    for doc in state_features:
+        for item in relo_list:
+            if item["secondary_state"] == doc["properties"]["name"]:
+                doc["properties"]["inflow"] = item["inflow"]
+                doc["properties"]["outflow"] = item["outflow"]
+    statesData["features"] = state_features
+    session.close()
+    return(jsonify(statesData))
+
+# @app.route('/api/statemigration')
+# def api_pull():
+    
+
+#     data = session.query(Relo).all()
+
+#     relo_list = []
+
+#     for row in data:
+#         dict = {}
+#         dict["primary_state"] = row.primary_state
+#         dict["secondary_state"] = row.secondary_state
+#         dict["inflow"] = row.inflow
+#         dict["outflow"] = row.outflow
+#         relo_list.append(dict)
+        
+#     state_features = statesData["features"]
+
+#     for doc in state_features:
+#         feature_list = []
+#         for item in relo_list:
+#             if item["secondary_state"] == doc["properties"]["name"]:
+#                 dict = {}
+#                 dict["primary_state"] = item["primary_state"]
+#                 dict["inflow"] = item["inflow"]
+#                 dict["outflow"] = item["outflow"]
+#                 feature_list.append(dict)
+#         doc["properties"]["flows"] = feature_list
+#     statesData["features"] = state_features
+
+#     session.close()
+
+#     return(jsonify(statesData))
 
 
     # document = {}
