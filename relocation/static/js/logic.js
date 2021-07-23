5a0e6385d4a8e74ec268fd129f7e39f8349b3ce3
Alivia Varvel <atvarvel@gmail.com>
2021-07-22 21:45:37 -0400

    updated css

--- relocation/static/js/logic.js
+++ relocation/static/js/logic.js
@@ -115,7 +115,7 @@ function updateStateIN (){
                     for (var i = 0; i < grades.length; i++) {
                         div.innerHTML +=
                         '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+                        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<p/>' : '+');
                     }
 
                     return div;
